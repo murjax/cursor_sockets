660d7306b738de58aa7c63be1c6ad66bc86ec9c3
Ryan Murphy <murjax@gmail.com>
2025-02-08 18:16:56 -0500

    Manage subscriptions on controller connect and disconnect

--- app/javascript/controllers/group_show_controller.js
+++ app/javascript/controllers/group_show_controller.js
@@ -2,22 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 import consumer from "channels/consumer"
 
 export default class extends Controller {
+  static targets = ["info"];
+
   connect() {
+    if (document.documentElement.hasAttribute("data-turbo-preview")) { return; }
+
     const groupId = this.data.get("group-id");
 
     const name = prompt("Enter your name");
     const sessionId = Math.floor(Math.random() * (3000 - 30) + 30);
 
-    let channel;
+    let subscription;
 
     const mouseMove = (event) => {
       let x = event.clientX;
       let y = event.clientY;
-      channel.send({ name, sessionId, x, y });
+      subscription.send({ name, sessionId, x, y });
     }
     this.mouseMove = mouseMove;
 
-    channel = consumer.subscriptions.create({ channel: "GroupChannel", group_id: groupId }, {
+    subscription = consumer.subscriptions.create({ channel: "GroupChannel", group_id: groupId }, {
       connected() {
         document.addEventListener("mousemove", mouseMove);
       },
@@ -46,10 +50,15 @@ export default class extends Controller {
         userDiv.style.top = (data.y + window.scrollY) + 'px';
       }
     });
+
+    this.subscription = subscription;
   }
 
   disconnect() {
     document.removeEventListener("mousemove", this.mouseMove);
+    if (this.subscription) {
+      consumer.subscriptions.remove(this.subscription);
+    }
   }
 
   generateVisibleColor() {
