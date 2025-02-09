d92b83e86c1465fc24ea1f659cbedefbf45a0537
Ryan Murphy <murjax@gmail.com>
2025-02-09 12:03:26 -0500

    Add chat window

--- app/javascript/controllers/group_show_controller.js
+++ app/javascript/controllers/group_show_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import consumer from "channels/consumer"
 
 export default class extends Controller {
-  static targets = ["info"];
+  static targets = ["info", "chatLog", "chatInput", "messageTemplate"];
 
   connect() {
     if (document.documentElement.hasAttribute("data-turbo-preview")) { return; }
@@ -10,16 +10,20 @@ export default class extends Controller {
     const groupId = this.data.get("group-id");
 
     const name = prompt("Enter your name");
+    this.name = name;
     const sessionId = Math.floor(Math.random() * (3000 - 30) + 30);
+    this.sessionId = sessionId;
 
     let subscription;
 
     const mouseMove = (event) => {
       let x = event.clientX;
       let y = event.clientY;
-      subscription.send({ name, sessionId, x, y });
+      subscription.send({ messageType: "cursor", name, sessionId, x, y });
     }
     this.mouseMove = mouseMove;
+    const messageTemplateTarget = this.messageTemplateTarget;
+    const chatLogTarget = this.chatLogTarget;
 
     subscription = consumer.subscriptions.create({ channel: "GroupChannel", group_id: groupId }, {
       connected() {
@@ -31,27 +35,57 @@ export default class extends Controller {
       },
 
       received(data) {
-        if (data.sessionId === sessionId) { return; }
+        if (data.messageType == "cursor") {
+          if (data.sessionId === sessionId) { return; }
 
-        let userDiv = document.getElementById(data.sessionId);
+          let userDiv = document.getElementById(data.sessionId);
 
-        if (!userDiv) {
-          userDiv = document.createElement("div");
-          userDiv.id = data.sessionId;
-          userDiv.style.width = '8px';
-          userDiv.style.height = '8px';
-          userDiv.style.backgroundColor = this.generateVisibleColor();
-          userDiv.style.borderRadius = '50%';
-          userDiv.style.position = 'absolute';
-          document.body.appendChild(userDiv);
+          if (!userDiv) {
+            userDiv = document.createElement("div");
+            userDiv.id = data.sessionId;
+            userDiv.style.width = '8px';
+            userDiv.style.height = '8px';
+            userDiv.style.backgroundColor = this.generateVisibleColor();
+            userDiv.style.borderRadius = '50%';
+            userDiv.style.position = 'absolute';
+            document.body.appendChild(userDiv);
+          }
+
+          userDiv.style.left = (data.x + window.scrollX) + 'px';
+          userDiv.style.top = (data.y + window.scrollY) + 'px';
+        } else if (data.messageType == "chat") {
+          const messageDiv = messageTemplateTarget.cloneNode(true);
+          delete messageDiv.dataset.groupShowTarget;
+          messageDiv.classList.remove("hidden");
+          messageDiv.classList.add("flex");
+          messageDiv.querySelector("div:nth-child(1)").innerHTML = data.sender;
+          messageDiv.querySelector("p").innerHTML = data.message;
+          messageDiv.querySelector("span").innerHTML = data.sentAt;
+          chatLogTarget.appendChild(messageDiv);
         }
-
-        userDiv.style.left = (data.x + window.scrollX) + 'px';
-        userDiv.style.top = (data.y + window.scrollY) + 'px';
       }
     });
 
     this.subscription = subscription;
+  }
+
+  chatInputTargetConnected() {
+    this.chatInputTarget.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        const message = this.chatInputTarget.value;
+        const messageId = Math.floor(Math.random() * (3000 - 30) + 30);
+        const now = new Date();
+        const sentAt = now.toLocaleString('en-US', {
+          hour: 'numeric',
+          minute: 'numeric',
+          hour12: true
+        });
+
+        const data = { messageType: "chat", message, messageId, sentAt, sender: this.name, sessionId: this.sessionId };
+        this.subscription.send(data);
+        this.chatInputTarget.value = "";
+      }
+    });
   }
 
   disconnect() {
